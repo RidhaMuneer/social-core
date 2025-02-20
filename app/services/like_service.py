3185a64eda8ed207ca91b47ecb70e8f8b1d37074
RidhaMuneer <ridhamuner@gmail.com>
2025-02-20 20:01:02 +0300

    ref: update likes in post when a user likes a post

--- app/services/like_service.py
+++ app/services/like_service.py
@@ -7,7 +7,7 @@ def like_service(like: Like, db: Session, current_user: User):
 
     if not post:
         return None
-    
+
     like_query = db.query(LikeModel).filter(
         LikeModel.post_id == like.post_id, LikeModel.user_id == current_user.id
     )
@@ -16,13 +16,17 @@ def like_service(like: Like, db: Session, current_user: User):
     if like.dir == 1:
         if found_like:
             return False
+        
         new_like = LikeModel(post_id=like.post_id, user_id=current_user.id)
         db.add(new_like)
+        post.likes += 1 
         db.commit()
         return True
-    else:
+    else: 
         if not found_like:
-            False
+            return False
+        
         like_query.delete(synchronize_session=False)
+        post.likes = max(0, post.likes - 1)
         db.commit()
         return True
