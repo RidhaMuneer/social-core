5852e4386c82bacb007f6d99ac15bba78a3ea9fa
RidhaMuneer <ridhamuner@gmail.com>
2025-02-21 18:06:41 +0300

    fix: using a proper type for current_user in follow request

--- app/routers/follow.py
+++ app/routers/follow.py
@@ -1,7 +1,7 @@
 from fastapi import status, Depends, APIRouter
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.schemas import FollowRequest
+from app.schemas import FollowRequest, User
 from app.oauth2 import get_current_user
 from app.controllers.follow_controller import follow_controller
 
@@ -11,6 +11,6 @@ router = APIRouter(prefix="/app/follow", tags=["Follow"])
 def follow(
     follow_request: FollowRequest,
     db: Session = Depends(get_db),
-    current_user: int = Depends(get_current_user),
+    current_user: User = Depends(get_current_user),
 ):
     return follow_controller(follow_request, db, current_user)
